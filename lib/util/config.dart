ba8075fa5a64513be9b43a8f03826398b9ba94b2
John ALLOU <allou.john@hotmail.fr>
2022-07-01 02:47:46 +0200

    ajout url prod

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,5 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-const String URL = "https://7373-2001-861-5e45-dcd0-c551-bbd1-e4e9-80a2.ngrok.io";
-// const String URL = "https://happytree.herokuapp.com";
+// const String URL = "https://7373-2001-861-5e45-dcd0-c551-bbd1-e4e9-80a2.ngrok.io";
+const String URL = "https://happytree.herokuapp.com";
 
